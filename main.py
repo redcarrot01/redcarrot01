e2de70eabd29bad98e385cdf35cb56573ca54d8f
redcarrot01 <38436013+redcarrot01@users.noreply.github.com>
2021-04-06 00:33:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ for i in feed['entries']:
         # dt = datetime.datetime.strptime(i['published'], "%a, %d %B %Y %H:%M:%S %z").strftime("%B %d, %Y")
         dt = i['published']
         markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
-        print(i['link'], i['title'],i['published'])
+        print(i['link'], i['title'])
 
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
