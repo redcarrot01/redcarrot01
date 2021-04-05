047ffb333d62853949723c5972d75d9a3efba746
redcarrot01 <38436013+redcarrot01@users.noreply.github.com>
2021-04-05 12:49:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,39 @@ velog_blog_rss_uri="https://v2.velog.io/rss/@redcarrot01"
 feed = feedparser.parse(velog_blog_rss_uri)
 
 markdown_text = """
+
+## Hi there! 👋
+
+### About myself 🥕
+
+I'm Yu Jin Hong and I'm interested in Backend development & Cloud.   
+I'm full with eager to learn new technique and skill.   
+I want to be a developer who grows day by day as much as github commits.   
+
+
+### Skill Set 
+
+✔ Familiar with  
+Python, c++, c, AWS, MySQL, mongondb, Crawling, Django, git
+
+🙌 Currently learning   
+Spring, Kubernetes
+
+✨ from now on & want to do well   
+Java, Spring, Spring boot, Spring cloud ..
+
+👌 So so & Had tried it  
+Html, css, Java, javascript, linux, Docker, Tensorflow, Android
+
+### About me & Contact me 
+
+  <a href="https://velog.io/@redcarrot01"><img src="https://img.shields.io/badge/Tech%20Blog-11B48A?style=flat-square&logo=Vimeo&logoColor=white&link=https://velog.io/@redcarrot01"/></a>  <a href="https://www.linkedin.com/in/yujin-hong-b93454193"><image src="https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/yujin-hong-b93454193"/></a>  <a href="mailto:redccc9010@gmail.com"><img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=viliketh1s98@naver.com"/></a> 
+
+
+[![redcarrot01's GitHub stats](https://github-readme-stats.vercel.app/api?username=redcarrot01&count_private=true&show_icons=true&theme=omni)](https://github.com/anuraghazra/github-readme-stats)
+
+<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fredcarrot01&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
+
 ### Recent blog posts
 """ # list of blog posts will be appended here
 
