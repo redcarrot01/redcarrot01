d91351be01e667886a5c294b769c6cd5139f4cf7
redcarrot01 <38436013+redcarrot01@users.noreply.github.com>
2021-04-05 12:21:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-# import feedparser, datetime
+import feedparser, datetime
 
-# velog_blog_rss_uri="https://v2.velog.io/rss/@redcarrot01"
-# feed = feedparser.parse(velog_blog_rss_uri)
+velog_blog_rss_uri="https://v2.velog.io/rss/@redcarrot01"
+feed = feedparser.parse(velog_blog_rss_uri)
 
-# markdown_text = """
-# ### Recent blog posts
-# """ # list of blog posts will be appended here
+markdown_text = """
+### Recent blog posts
+""" # list of blog posts will be appended here
 
-# lst = []
+lst = []
 
+j=0
+for i in feed['entries']:
+    j+= 1
+    if j >5:
+        break
+    else:
+        # dt = datetime.datetime.strptime(i['published'], "%a, %d %B %Y %H:%M:%S %z").strftime("%B %d, %Y")
+        dt = i['published']
+        markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+        print(i['link'], i['title'],i['published'])
 
-# for i in feed['entries']:
-#     # dt = datetime.datetime.strptime(i['published'], "%a, %d %B %Y %H:%M:%S %z").strftime("%B %d, %Y")
-#     dt = i['published']
-#     markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
-#     print(i['link'], i['title'])
-
-# f = open("README.md",mode="w", encoding="utf-8")
-# f.write(markdown_text)
-# f.close()
+f = open("README.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
